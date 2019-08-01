474bf63a8798fda35f5bfcf95cfc9f930b83eb88
Daniel Miller <danielmiller5791@gmail.com>
2019-08-01 22:07:31 +0100

    staging copy done moving to insert and refactoring

--- test_file.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO song_staging VALUES (
-  "AR0S3YD1187B99967F',,"Jacksonville, FL",,"The Classics
-IV",127.65995,1,"SOYRACV12A8C13C4D8","24 Hours Of Loneliness",1975
-);
--- test_re.py
+++
@@ -1,8 +0,0 @@
-import ftfy
-import re
-
-stri = "S\ufffdPaulo, Brazil"
-
-regex = r'([^\s\w]+)'
-
-print(re.sub(regex, '', stri))
